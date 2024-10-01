8bf65d91b8595236f883286fae87d308f43411ee
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-10-01 13:19:03 +0100

    Añadimos los textos de Usuario y Contraseña y los alineamos en el medio con su respectivo tamaño

--- app/src/main/java/com/example/gestorcontrasenas/MainActivity.kt
+++ app/src/main/java/com/example/gestorcontrasenas/MainActivity.kt
@@ -3,13 +3,39 @@ package com.example.gestorcontrasenas
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello Android!")
+            MainContent()
         }
+    }
+}
+
+@Composable
+fun MainContent() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Usuario:",
+            style = TextStyle(fontSize = 30.sp)
+        )
+        Text(
+            text = "Contraseña:",
+            style = TextStyle(fontSize = 30.sp)
+        )
     }
 }
