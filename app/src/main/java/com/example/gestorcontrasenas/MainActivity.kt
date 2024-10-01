46fd9032d50f2379fee8739a595d6224d6e50fab
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-10-01 13:25:52 +0100

    Añadimos el título y le añadimos padding

--- app/src/main/java/com/example/gestorcontrasenas/MainActivity.kt
+++ app/src/main/java/com/example/gestorcontrasenas/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.gestorcontrasenas
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
@@ -27,15 +27,26 @@ fun MainContent() {
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.Top
     ) {
         Text(
-            text = "Usuario:",
-            style = TextStyle(fontSize = 30.sp)
+            text = "Gestor de contraseñas",
+            style = TextStyle(fontSize = 37.sp, fontWeight = FontWeight.Bold),
+            modifier = Modifier.padding(top = 36.dp)
         )
-        Text(
-            text = "Contraseña:",
-            style = TextStyle(fontSize = 30.sp)
-        )
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Usuario:",
+                style = TextStyle(fontSize = 28.sp, fontWeight = FontWeight.Bold)
+            )
+            Text(
+                text = "Contraseña:",
+                style = TextStyle(fontSize = 28.sp, fontWeight = FontWeight.Bold)
+            )
+        }
     }
 }
