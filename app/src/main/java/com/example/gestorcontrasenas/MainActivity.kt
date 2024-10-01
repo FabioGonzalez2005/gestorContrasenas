1f06eb85bd1e4a7ee938812d409a47aeeff1adf8
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-10-01 13:40:18 +0100

    Añadido Preview

--- app/src/main/java/com/example/gestorcontrasenas/MainActivity.kt
+++ app/src/main/java/com/example/gestorcontrasenas/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -49,4 +50,11 @@ fun MainContent() {
             )
         }
     }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun AppPreview() {
+    MainContent ()
 }
